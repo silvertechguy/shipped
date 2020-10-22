b166f7bfbf988d0cd34d953bdb8a4783a19c6a9b
Ali Fadda <silvertechguy@gmail.com>
2020-10-22 23:33:31 +0200

    add to cart functionality

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actions/cartActions";
 
-const CartScreen = () => {
+const CartScreen = ({ match, location }) => {
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
